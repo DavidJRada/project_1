4d8864e07b8ace0362a6e9690d99a24a95e74e0b
davidjrada <davidjrada@gmail.com>
2019-04-05 16:45:53 -0400

    able to pull forecast Infomation and display it

--- js/main.js
+++ js/main.js
@@ -9,21 +9,43 @@ const requestData = () => {
 
         }
     }).then(function (data) {
-        $('<div>').text(data.main.temp).appendTo('.weatherDisplay')
+        $('<div>').text(Math.floor(data.main.temp) + '°f').appendTo('.weatherDisplay')
+
+        $('<div>').text('Humidity: ' + data.main.humidity + '%').appendTo('.weatherDisplay')
+
         for (let i = 0; i < (data.weather).length; i++) {
             console.log(data)
             // console.log(data.weather[i].id)
 
-            $('<div>').text(data.weather[i].description).appendTo('.weatherDisplay')
+            $('<div>').text('Current weather: ' + data.weather[i].description).appendTo('.weatherDisplay')
+        }
+
+    })
+}
+
+const requestForecast = () => {
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&APPID=0d4a536dd84f2c41a282e010c8caaf60",
+        type: "GET",
+        data: {
+
+        }
+    }).then(function (data) {
+        for (let i = 0; i < 5; i++) {
+            console.log(data.list[i])
+            // console.log(data.weather[i].id)
+
+            $('<div>').text(data.list[i].main.temp).appendTo('.forecast')
         }
     })
 }
 
-
 const getTask = () => {
+    $('.taskList').empty()
+
     taskInput = $('#input').val()
 
-    let todoItem = $('<div>').addClass('toDoItems').text(taskInput)
+    let todoItem = $('<div>').addClass('items').addClass('toDoItems').text(taskInput)
 
     todoItem.appendTo('.tasks')
 
@@ -42,9 +64,15 @@ $(() => {
         e.preventDefault();
         requestData();
     })
+    $('#getForecast').on('click', (e) => {
+        e.preventDefault();
+        requestForecast();
+    })
 
     $('form').on('submit', (e) => {
         e.preventDefault()
+        $('#input').empty()
+        $('.taskList').empty()
         getTask()
     })
 
