bd5ee7ec5c9c355e2b725b15bd47a86acad8560a
davidjrada <davidjrada@gmail.com>
2019-04-07 15:01:42 -0400

    starting HTML and CSS over from scratch

---
+++ README.md
@@ -0,0 +1,4 @@
+Difficult to matcht he forecast descritption with the corresponding icon. I am having trouble deciding which data method is best for the icons. 
+    An object or array of objects?
+    Attempted to work on this for 2 hours and could not find a solution. Decided to work on something else
+Working on a redesign of the site to focus more on weather info and less on to do list
--- js/main.js
+++ js/main.js
@@ -23,9 +23,18 @@ const requestData = () => {
     })
 }
 
-const weatherIcons = {
-
+const weatherIconsObj = {
+    'clouds': 'weather_icons/clouds.png',
+    'Clear': 'weather_icons/clear.png',
+    'rain': 'weather_icons/rain.png'
 }
+
+const weatherIconsArr = [
+
+    { 'Clouds': 'weather_icons/clouds.png' },
+    { 'Clear': 'weather_icons/clear.png' },
+    { 'Rain': 'weather_icons/rain.png' }
+]
 
 const requestForecast = () => {
     $.ajax({
@@ -35,34 +44,80 @@ const requestForecast = () => {
 
         }
     }).then(function (data) {
-        console.log(data)
-        for (let i=0; i<data.list.length; i++) {
-            console.log(data.list[i].weather[0].main)
-        }
+        // console.log(data)
+        //Loop through forecast list data to pull out the main description of the weather
+        // for (let i = 0; i < data.list.length; i++) {
+        //     //Store the result of hourly forecast as variable
+        //     let hourlyForecast = data.list[i].weather[0].main
+        //     // for (let j = 0; j < weatherIconsArr.length; j++) {
+        //     if (weatherIconsArr.filter(icon => {
+        //         return icon === hourlyForecast
+        //     }))
+
+
+        //         // return(match)
+        //     }
+    })
 
 
 
 
 
-            // for (let i = 0; i < ; i++) {
-            // for(let j=0; i<(data.list.length); i++)
-            // console.log(data.list.weather[i].main)
 
 
-            // console.log(data[i].forecast.symbol.name)
-            // let $forecastItem = $('<div>').addClass('forecastItem')
-            // let $temp = $('<div>').text(data.list[i].main.temp)
-            // let $time = $('<div>').text(((data.list[i].dt_txt).split(' '))[1])
-            // console.log($time)
-            
 
-            // $forecastItem.appendTo('.forecast')
-            // $temp.appendTo($forecastItem)
-            // $time.appendTo($forecastItem)
 
-        }
-    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Match forecast weather description with the corresponding object in the array of Objects(weatherIcons)
+    // for (let j = 0; j < weatherIcons.length; j++) {
+    //     if(weatherIcons[j].find(hourlyForecast)) {
+    //         console.log(weatherIcons[j])
+    //     }
+    // console.log(hourlyForecast)
+    // console.log(weatherIcons.filter(hourlyForecast.toString()))
+
+    // if(hourlyForecast === 'Clear') {
+    //     let hourlyForecastDiv = $('<div>').addText(hourlyForecast)
+
+    //     console.log(weatherIcons['Clear'])
+    // }
+    // }
+
+
+
+
+
+
+    // for (let i = 0; i < ; i++) {
+    // for(let j=0; i<(data.list.length); i++)
+    // console.log(data.list.weather[i].main)
+
+
+    // console.log(data[i].forecast.symbol.name)
+    // let $forecastItem = $('<div>').addClass('forecastItem')
+    // let $temp = $('<div>').text(data.list[i].main.temp)
+    // let $time = $('<div>').text(((data.list[i].dt_txt).split(' '))[1])
+    // console.log($time)
+
+
+    // $forecastItem.appendTo('.forecast')
+    // $temp.appendTo($forecastItem)
+    // $time.appendTo($forecastItem)
+
 }
+
 
 const getTask = () => {
     $('.taskList').empty()
